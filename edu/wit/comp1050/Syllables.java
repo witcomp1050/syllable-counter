13d283b70d1fd3e1117d14100eae2bffcfeef0d2
Ramon Asuncion <asuncionbatista@gmail.com>
2020-09-18 18:24:58 -0400

    Push to work on second computer

--- edu/wit/comp1050/Syllables.java
+++ edu/wit/comp1050/Syllables.java
@@ -18,7 +18,7 @@ public class Syllables
     public static int getSyllableCount(String word)
     {
         // Initiates the syllableCount variable as a integer
-        int syllableCount = 0;
+        int syllableCount = 1;
         String lowercaseWord = word.toLowerCase();
 
         if (wordIsEmpty(word)) {
@@ -26,30 +26,35 @@ public class Syllables
             return syllableCount = 0;
         }
         // For loop for the length of the word
-        for (int i = 0; i < lowercaseWord.length(); i++) {
+        for (int i = 0; i < lowercaseWord.length(); i++)
+        {
             // Checks if string has a vowel
             if (lowercaseWord.charAt(i) == 'a'
                 || lowercaseWord.charAt(i) == 'e'
                 || lowercaseWord.charAt(i) == 'i'
                 || lowercaseWord.charAt(i) == 'o'
-                || lowercaseWord.charAt(i) == 'u') {
+                || lowercaseWord.charAt(i) == 'u')
+            {
                 // Adds one if true
                 syllableCount++;
             }
         }
        // If the word does not start with Y, however, contains the letter y
-       if (lowercaseWord.charAt(0) != 'y' && lowercaseWord.contains("y")) {
+       if (lowercaseWord.charAt(0) != 'y' && lowercaseWord.contains("y"))
+       {
            // Add one to syllableCount
            syllableCount++;
        }
         // If last letter of the word is a e then subtract 1.
-        if (lowercaseWord.charAt(lowercaseWord.length() - 1 ) == 'e'){
+        if (lowercaseWord.charAt(lowercaseWord.length() - 1 ) == 'e')
+        {
             // Subtract one to syllableCount
             syllableCount--;
         }
 
         // Reads the triphthongs.txt file
-        try {
+        try
+        {
             File file = new File("triphthongs.txt");
             Scanner readTriphthongsFile = new Scanner(file);
             while (readTriphthongsFile.hasNextLine()) {
@@ -60,14 +65,16 @@ public class Syllables
                     syllableCount--;
                 }
             }
-        } catch (FileNotFoundException e){
+        } catch (FileNotFoundException e)
+        {
             // If file not found print string below.
             out.print("Attempt to open the triphthongs.txt file has failed. File does not exist.");
             // Exits out of the program
             exit(0);
         }
         // Reads the diphthongs.txt file.
-        try {
+        try
+        {
             File file = new File("diphthongs.txt");
             Scanner readDiphthongsFile = new Scanner(file);
             while (readDiphthongsFile.hasNextLine()) {
@@ -78,7 +85,8 @@ public class Syllables
                     syllableCount--;
                 }
             }
-        } catch (FileNotFoundException e){
+        } catch (FileNotFoundException e)
+        {
             // If file not found print string below
             out.print("Attempt to open the diphthongs.txt file has failed. File does not exist.");
             // Exits out of the program
@@ -87,8 +95,20 @@ public class Syllables
 
         // Checks if the word ends with le or les
 
-        if (lowercaseWord.endsWith("le") || lowercaseWord.endsWith("les")){
+        if (lowercaseWord.endsWith("le") || lowercaseWord.endsWith("les"))
+        {
+
         }
+
+        if (lowercaseWord.startsWith("io"))
+        {
+            syllableCount++;
+        }
+        if (lowercaseWord.endsWith("ee") || lowercaseWord.endsWith("ie"))
+        {
+            syllableCount++;
+        }
+
 
 
 

6741d7ac70695b90f79880ac9b996c63caba1602
Ramon Asuncion <asuncionbatista@gmail.com>
2020-09-23 23:48:36 -0400

    Add comments to script

--- edu/wit/comp1050/Syllables.java
+++ edu/wit/comp1050/Syllables.java
@@ -26,29 +26,32 @@ public class Syllables
             // Returns 0 if word is null or empty from the helper class
             return syllableCount = 0;
         }
+
         // For loop for the length of the word
         for (int i = 0; i < lowercaseWord.length(); i++)
         {
-            // Checks if string has a vowel
+            //Counts the number of vowels (a, e, i, o, u) in the word.
             if (lowercaseWord.charAt(i) == 'a' || lowercaseWord.charAt(i) == 'e' || lowercaseWord.charAt(i) == 'i' || lowercaseWord.charAt(i) == 'o' || lowercaseWord.charAt(i) == 'u')
             {
-                // Adds one if true
+                // Add one to syllableCount
                 syllableCount++;
             }
         }
-       // If the word does not start with Y, however, contains the letter y
+
+       // Add 1 every time the letter y makes the sound of a vowel. (When y is not the first letter in the word.)
        if (lowercaseWord.charAt(0) != 'y' && lowercaseWord.contains("y"))
        {
            // Add one to syllableCount
            syllableCount++;
        }
-        // If last letter of the word is a e then subtract 1.
+        // If last letter of the word is a e then subtract 1 (silent e at the end of a word)
         if (lowercaseWord.charAt(lowercaseWord.length() - 1 ) == 'e')
         {
             // Subtract one to syllableCount
             syllableCount--;
         }
 
+        // Subtract 1 for each diphthong or triphthong in the word
         // Reads the triphthongs.txt file
         try
         {
@@ -69,6 +72,7 @@ public class Syllables
             // Exits out of the program
             exit(0);
         }
+
         // Reads the diphthongs.txt file.
         try
         {
@@ -92,19 +96,24 @@ public class Syllables
 
         // Checks if the word ends with le or les and if the letter before is a consonant
         if (lowercaseWord.endsWith("le") || lowercaseWord.endsWith("les")){
+            // Gets the character before attached to the le
             String checkFirstLetterVowel = lowercaseWord.substring(lowercaseWord.lastIndexOf("le") - 1);
-            out.println(checkFirstLetterVowel);
+            // Checks if the string has a vowel in it
             if (!(checkFirstLetterVowel.startsWith("a") ||checkFirstLetterVowel.startsWith("e") ||checkFirstLetterVowel.startsWith("i") || checkFirstLetterVowel.startsWith("o") || checkFirstLetterVowel.startsWith("u"))) {
+                // Add one to syllableCount
                 syllableCount++;
             }
         }
-
+        // If the word starts with io --> add 1 to the syllable count.
         if (lowercaseWord.startsWith("io"))
         {
+            // Add one to syllableCount
             syllableCount++;
         }
+        // If the word ends with ee or ie --> add 1 to the syllable count.
         if (lowercaseWord.endsWith("ee") || lowercaseWord.endsWith("ie"))
         {
+            // Add one to syllableCount
             syllableCount++;
         }
 

d6b970c1fbf734a25afd3a160df8e81a48ae5187
Ramon Asuncion <asuncionbatista@gmail.com>
2020-09-24 00:58:32 -0400

    New method of checking if word ends with les and le and check the character before

--- edu/wit/comp1050/Syllables.java
+++ edu/wit/comp1050/Syllables.java
@@ -13,27 +13,27 @@ public class Syllables
 
     public static void main(String[] args)
     {
-        // what prints out to the console
+        // what prints out to the console.
     }
 
     public static int getSyllableCount(String word)
     {
-        // Initiates the syllableCount variable as a integer
-        int syllableCount = 1; // Subject to change due to some words ending up at 0
+        // Initiates the syllableCount variable as a integer.
+        int syllableCount = 0; // Subject to change due to some words ending up at 0.
         String lowercaseWord = word.toLowerCase();
 
         if (wordIsEmpty(word)) {
-            // Returns 0 if word is null or empty from the helper class
+            // Returns 0 if word is null or empty from the helper class.
             return syllableCount = 0;
         }
 
-        // For loop for the length of the word
+        // For loop for the length of the word.
         for (int i = 0; i < lowercaseWord.length(); i++)
         {
             //Counts the number of vowels (a, e, i, o, u) in the word.
             if (lowercaseWord.charAt(i) == 'a' || lowercaseWord.charAt(i) == 'e' || lowercaseWord.charAt(i) == 'i' || lowercaseWord.charAt(i) == 'o' || lowercaseWord.charAt(i) == 'u')
             {
-                // Add one to syllableCount
+                // Add one to syllableCount.
                 syllableCount++;
             }
         }
@@ -41,18 +41,18 @@ public class Syllables
        // Add 1 every time the letter y makes the sound of a vowel. (When y is not the first letter in the word.)
        if (lowercaseWord.charAt(0) != 'y' && lowercaseWord.contains("y"))
        {
-           // Add one to syllableCount
+           // Add one to syllableCount.
            syllableCount++;
        }
-        // If last letter of the word is a e then subtract 1 (silent e at the end of a word)
+        // If last letter of the word is a e then subtract 1 (silent e at the end of a word).
         if (lowercaseWord.charAt(lowercaseWord.length() - 1 ) == 'e')
         {
-            // Subtract one to syllableCount
+            // Subtract one to syllableCount.
             syllableCount--;
         }
 
-        // Subtract 1 for each diphthong or triphthong in the word
-        // Reads the triphthongs.txt file
+        // Subtract 1 for each diphthong or triphthong in the word.
+        // Reads the triphthongs.txt file.
         try
         {
             File file = new File("triphthongs.txt");
@@ -69,7 +69,7 @@ public class Syllables
         {
             // If file not found print string below.
             out.print("Attempt to open the triphthongs.txt file has failed. File does not exist.");
-            // Exits out of the program
+            // Exits out of the program.
             exit(0);
         }
 
@@ -88,41 +88,55 @@ public class Syllables
             }
         } catch (FileNotFoundException e)
         {
-            // If file not found print string below
+            // If file not found print string below.
             out.print("Attempt to open the diphthongs.txt file has failed. File does not exist.");
-            // Exits out of the program
+            // Exits out of the program.
             exit(0);
         }
 
-        // Checks if the word ends with le or les and if the letter before is a consonant
-        if (lowercaseWord.endsWith("le") || lowercaseWord.endsWith("les")){
-            // Gets the character before attached to the le
-            String checkFirstLetterVowel = lowercaseWord.substring(lowercaseWord.lastIndexOf("le") - 1);
-            // Checks if the string has a vowel in it
-            if (!(checkFirstLetterVowel.startsWith("a") ||checkFirstLetterVowel.startsWith("e") ||checkFirstLetterVowel.startsWith("i") || checkFirstLetterVowel.startsWith("o") || checkFirstLetterVowel.startsWith("u"))) {
+        // Checks if the word ends with le.
+        if (lowercaseWord.endsWith("le")) {
+            // Gets the character before attached to the le.
+            String leBeforeIfConsonant = lowercaseWord.substring(lowercaseWord.lastIndexOf("le") - 1);
+            // Checks if it contains a vowel if not then continue if statement.
+            if (!(leBeforeIfConsonant.startsWith("a") || leBeforeIfConsonant.startsWith("e") || leBeforeIfConsonant.startsWith("i") || leBeforeIfConsonant.startsWith("o") || leBeforeIfConsonant.startsWith("u"))) {
                 // Add one to syllableCount
                 syllableCount++;
             }
         }
+
+        // Checks if the word ends with les.
+        if (lowercaseWord.endsWith("les")) {
+        // Gets the character before attached to the le.
+        String lesBeforeIfConsonant = lowercaseWord.substring(lowercaseWord.lastIndexOf("les") - 1);
+        // Checks if it contains a vowel if not then continue if statement.
+        if (!(lesBeforeIfConsonant.startsWith("a") || lesBeforeIfConsonant.startsWith("e") || lesBeforeIfConsonant.startsWith("i") || lesBeforeIfConsonant.startsWith("o") || lesBeforeIfConsonant.startsWith("u"))) {
+            // Add one to syllableCount.
+            syllableCount++;
+        }
+    }
+
+
         // If the word starts with io --> add 1 to the syllable count.
         if (lowercaseWord.startsWith("io"))
         {
-            // Add one to syllableCount
+            // Add one to syllableCount.
             syllableCount++;
         }
         // If the word ends with ee or ie --> add 1 to the syllable count.
         if (lowercaseWord.endsWith("ee") || lowercaseWord.endsWith("ie"))
         {
-            // Add one to syllableCount
+            // Add one to syllableCount.
             syllableCount++;
         }
 
-        // Returns the syllableCount to the getSyllableCount function
+        // Returns the syllableCount to the getSyllableCount function.
         return syllableCount;
     }
 
     // Helper class to check if the String word is null or empty.
     private static boolean wordIsEmpty(String word) {
+        // Checks if the word is null or is a empty string.
         return word == null || word.isEmpty();
     }
 }
